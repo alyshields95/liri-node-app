09547dbf3854550625bc8ee25fbd6a6d22f14763
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-09-06 13:04:06 -0400

    updating

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//user inputs.
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
